3a294e7c56c38918011df63d4fd0294a6a31a040
Vitor Falcao <joaofalcao.33@outlook.com>
2023-05-02 15:36:54 -0300

    notification page ready for deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
 import Profile from './components/Profile'
 
 function App() {
   return (
     <>
       <div className="container">
-        
         <Profile />
       </div>
     </>
